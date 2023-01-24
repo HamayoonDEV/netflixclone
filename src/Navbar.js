7a0e845a62915ffc3c717e2522fb23df1578abda
unknown <hamayoon.47611@iqra.edu.pk>
2023-01-25 02:18:56 +0500

    added styles to navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 
 const Navbar = () => {
+ const [show,handelshow] = useState(false);
+
+ const transitionScrollBar = ()=>{
+    if(window.scrollY > 100){
+        handelshow(false)
+    }
+    else{
+        handelshow(true)
+    }
+ }
+useEffect(()=>{
+    window.addEventListener("scroll",transitionScrollBar)
+    return () => window.removeEventListener("scroll",transitionScrollBar)
+},[])
+
   return (
-    <div className='navbar' >
-        <h1>this is a navbar</h1>
+<div className={`navbar  ${show && "nav-black"}`} >
+    <div className='nav-contents' >        
+        <img className='nav-logo' 
+             alt='netflix' 
+             src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png' />
+        
+        <img className='nav-avatar'  
+              alt='avatar'
+              src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png' />
     </div>
+
+</div>
   )
 }
 
