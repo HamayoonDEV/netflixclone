1e5965309cb1b0b8178b71a7eeddd8fa3585087c
unknown <hamayoon.47611@iqra.edu.pk>
2023-01-25 01:28:17 +0500

    add HomeScreen fike

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDvoueWQJEqQXvvEhEiHuUx53W_NTEbGoY",
+    authDomain: "netflix-clone-aa0a3.firebaseapp.com",
+    projectId: "netflix-clone-aa0a3",
+    storageBucket: "netflix-clone-aa0a3.appspot.com",
+    messagingSenderId: "888491036615",
+    appId: "1:888491036615:web:e90c3f3dd1ddf7f26593e2"
+  };
