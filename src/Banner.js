950d76da60566a50d6036d8e216459487976e4de
unknown <hamayoon.47611@iqra.edu.pk>
2023-01-25 15:22:19 +0500

    added styles to the banner.css file and little bit updates in banner.js file

--- src/Banner.js
+++ src/Banner.js
@@ -3,7 +3,20 @@ import './Banner.css'
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <header className='banner' > 
+
+    <div className='banner-content'>
+        <h1 className='banner-title' > movie name</h1>
+        <div className='banner-buttons' >
+            <button className='banner-button'>play</button>
+            <button className='banner-button' >My list</button>
+        </div>
+        <h1 className='banner-description'> 
+        this is a test description
+         </h1>
+    </div>
+    <div className='banner-fadebutton' />
+    </header>
   )
 }
 
