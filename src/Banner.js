7f80d350b3c4936c90cc59ec5dfcbe0e2a6c76cb
unknown <hamayoon.47611@iqra.edu.pk>
2023-01-27 00:03:47 +0500

    added axios file and request file to fetch the data from tmdb database of movies

--- src/Banner.js
+++ src/Banner.js
@@ -1,21 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Banner.css'
+import axios from './axios'
+import requests from './request'
 
 const Banner = () => {
+        const [movies,setMovies] = useState([])
+
+        useEffect(()=>{
+            async function fetchData(){
+                const request = await axios.get(requests.fetchTv)
+                setMovies(
+                    request.data.results[
+                        Math.floor(Math.random() * request.data.results.length-1)
+                    ]
+                )
+                return request;
+            }
+             fetchData();
+        },[])
+        console.log(movies)
+
     function truncate(string,n){
-        return string.length > n ? string.substr(0,n-1) + "..." : string;
+        return string?.length > n ? string.substr(0,n-1) + "..." : string;
     }
   return (
     <header className='banner' > 
 
     <div className='banner-content'>
-        <h1 className='banner-title' > movie name</h1>
+        <h1 className='banner-title' > {movies.name}</h1>
         <div className='banner-buttons' >
             <button className='banner-button'>play</button>
             <button className='banner-button' >My list</button>
         </div>
         <h1 className='banner-description'> 
-        {truncate(`this is my description`,150)}
+        {truncate(movies.overview,150)}
          </h1>
     </div>
     <div className='banner-fadebutton' />
--- src/request.js
+++ src/request.js
@@ -2,10 +2,11 @@
 
 const API_KEY = "568e8426f23a80efe494302ab02aa346";
 
-const request = {
-    fetchTrending:`/trending/all/week?api_key=${API_KEY}&lanhuage=en-US`,
+const requests = {
+    fetchTv:`/tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
     fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
 
 }
 
-export default request;
+export default requests;
+//https://api.themoviedb.org/3/tv/popular?api_key=568e8426f23a80efe494302ab02aa346&language=en-US&page=1
